7fca7b909e5b2f5a594e452996de496c019b46b4
eeshmidha <eeshmidha80@gmail.com>
2025-08-14 19:34:06 +0530

    Refactor :Categories API logic

--- app/api/categories/routes.ts
+++ app/api/categories/routes.ts
@@ -1,22 +1,43 @@
 import { NextResponse,NextRequest } from "next/server";
-import { getCategoriesFromDatabase } from "@/lib/database";
-export async function get(){
-    try {
-        const categories = await getCategoriesFromDatabase();
-        return NextResponse.json(
-            {
-                categories:categories.map((c)=>c)
-            },
-            {
-                status:200,
-                headers:{
-                    "Cache-Control":"public"
-                }
+import { getAllCategories,getCategoryBySlug } from "@/lib/database";
+
+export default async  function GET(req:NextRequest){
+    try{
+        const {searchParams} = new URL(req.url)
+        const slug =searchParams.get('slug')
+
+        if(slug){
+            const result = await getCategoryBySlug(slug)
+
+            if(result.error){
+                return NextResponse.json(
+                    {success:false,error:result.error,data:null},
+                    {status:404}
+                )
             }
+
+            return NextResponse.json(
+                {success:true,error:null,data:result.data},
+                {status:200}
+            )
+        }
+        const result = await getAllCategories();
+
+        if(result.error){
+            return NextResponse.json(
+                {succes:false,error:result.error,data:null},
+                {status:404}
+            )
+        }
+         return NextResponse.json(
+            {success:true,error:null,data:result.data},
+            {status:200}
         )
-    }
-    catch(error){
-        console.log("Error fetching categories",error);
-        return NextResponse.json({error:"Failed to load categories from database"} , {status : 500})
+    }catch(error){
+        console.error("Categories API error", error)
+        return NextResponse.json(
+            { success: false, error: (error as Error).message, data: null },
+            { status: 500 }
+        )
     }
 }
