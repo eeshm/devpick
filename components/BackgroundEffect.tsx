6d8dd86b6c2f7588ab984461a8c6b7cca5643399
eeshmidha <eeshmidha80@gmail.com>
2025-08-31 16:39:56 +0530

    background shading from dark to light

--- components/BackgroundEffect.tsx
+++ components/BackgroundEffect.tsx
@@ -4,9 +4,9 @@ export default function BackgroundEffect() {
     <div
       className="fixed bottom-0 left-0 w-full min-h-screen -z-10"
       style={{
-        background: 'radial-gradient(1920px 100% at 50% 100%, rgba(155, 28, 49, 0.6) 0%, rgba(80, 56, 255, 0.5) 50%, transparent 100%)',
-        maskImage: 'radial-gradient(1920px 100% at 50% 100%, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.5) 50%, transparent 100%)',
-        WebkitMaskImage: 'radial-gradient(1920px 100% at 50% 100%, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.5) 50%, transparent 100%)', // For Webkit browsers
+        background: 'radial-gradient(1920px 100% at 50% 100%, rgba(155, 28, 49, 0.6) 0%, rgba(80, 56, 255,0.7) 50%, transparent 100%)',
+        maskImage: 'radial-gradient(1920px 100% at 50% 100%, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.7) 100%, transparent 100%)',
+        WebkitMaskImage: 'radial-gradient(1920px 100% at 50% 100%, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0.7) 100%, transparent 100%)', // For Webkit browsers
         opacity: 0.9,
       }}
     ></div>
