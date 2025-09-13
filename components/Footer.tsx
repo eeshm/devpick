0b10bbf247e85bf6291c5b5f7054e6a989af3869
eeshmidha <eeshmidha80@gmail.com>
2025-09-14 00:17:19 +0530

    feat:update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,26 @@
 // import TwitterIcon from "./icons/Twitter";
 import Link from "next/link";
-import { Heart } from "lucide-react";
+import { Briefcase, Github, Heart } from "lucide-react";
+import { LinkedinIcon } from "lucide-react";
 
 import TwitterIcon from "./icons/Twitter";
 export default function Footer() {
     return (
-        <footer className=" px-4 mt-10 mb-2 text-white">
+        <footer className=" mt-15 px-2 text-lg mb-2 text-white">
             <div className=" container mx-auto text-center flex justify-between">
                 <div className="flex items-center  justify-center space-x-1">
                     <span className="text-white">Made with</span>
                     <Heart className="fill-white"/>
+                    <span>by eesh</span>
                 </div>
+                <div className="flex gap-3">
                 <Link href="https://x.com/eeshmidha1" target="_blank" rel="noopener noreferrer">
                     <TwitterIcon/>
                 </Link>
+                <Link href="https://github.com/eeshm" target="_blank" rel="noopener noreferrer">
+                <Github/>
+                </Link>
+                </div>
             </div>
         </footer >
     )
